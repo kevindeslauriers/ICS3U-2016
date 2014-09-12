1949ad1c85197881885c671a51f1528da01c0ca5
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-09-12 11:23:58 -0400

    Solution

---
+++ Unit One/src/com/bayviewglen/dayfour/SixtyPointQuizSolution.java
@@ -0,0 +1,16 @@
+package com.bayviewglen.dayfour;
+
+public class SixtyPointQuizSolution {
+
+	public static void main(String[] args) {
+		int x, y, z;
+		
+		x = 2;
+		y = 3;
+		z = x + y;
+		
+		System.out.println(x + "+" + y + "=" + z);
+
+	}
+
+}
