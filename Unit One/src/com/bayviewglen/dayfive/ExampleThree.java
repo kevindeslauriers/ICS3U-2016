17d9f8e5ac70ff872ca4fd0e8771a487ff28bb43
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-09-16 09:37:10 -0400

    Initial Commit

--- Unit One/src/com/bayviewglen/dayfive/ExampleThree.java
+++ Unit One/src/com/bayviewglen/dayfive/ExampleThree.java
@@ -8,10 +8,25 @@
 
 package com.bayviewglen.dayfive;
 
+import java.util.Scanner;
+
 public class ExampleThree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Please enter a three (3) digit number: ");
+		
+		int number = keyboard.nextInt();
+		
+		int digit1 = number % 10;
+		int digit3 = number / 100;
+		int digit2 = number % 100 / 10;
+		
+		System.out.println("The sum of the numbers " + digit3 + ", " + digit2 + " and " + digit1 + " is " + (digit1 + digit2 + digit3));
+		
+		keyboard.close();
+		
 
 	}
 
