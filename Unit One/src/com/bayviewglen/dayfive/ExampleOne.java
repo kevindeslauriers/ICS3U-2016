71bc27b75ff26f438e35df9eeeeadaeb4a655aca
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-09-16 09:26:35 -0400

    Initial Commit

--- Unit One/src/com/bayviewglen/dayfive/ExampleOne.java
+++ Unit One/src/com/bayviewglen/dayfive/ExampleOne.java
@@ -1,17 +1,33 @@
 /*
+
  * Declaring a Scanner to read input from the Keyboard.
  * 
  * Reading integer data.
  * 
- * Prompt the user for three different integers and display the average as the three numbers.
+ * Prompt the user for three different integers and display the average of the three numbers.
  */
 package com.bayviewglen.dayfive;
+
+import java.util.Scanner;
 
 public class ExampleOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
 
+		int x, y, z;
+		System.out.print("Please enter three integers: ");
+		
+		x = keyboard.nextInt(); // Grabs the next integer
+		y = keyboard.nextInt(); // Grabs the next integer
+		z = keyboard.nextInt(); // Grabs the next integer
+		
+		double average = (x + y + z) / 3.0;
+		
+		System.out.println("The average of the numbers: " + x + ", " + y + " and " + z + " is " + average);
+		
+		keyboard.close();
+		
 	}
 
 }
--- Unit One/src/com/bayviewglen/dayfive/ExampleTwo.java
+++ Unit One/src/com/bayviewglen/dayfive/ExampleTwo.java
@@ -12,10 +12,31 @@
 
 package com.bayviewglen.dayfive;
 
+import java.util.Scanner;
+
 public class ExampleTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in); 
+		
+		double length, width, height;
+		double surfaceArea;
+		
+		System.out.print("Please enter the length: ");
+		length = in.nextDouble();
+		
+		System.out.print("Please enter the width: ");
+		width = in.nextDouble();
+		
+		System.out.print("Please enter the height: ");
+		height = in.nextDouble();
+		
+		surfaceArea = (2 * length * width) + (2 * width * height) + (2 * length * height);
+		
+		System.out.println("The surface area of a box with (width, length, height) of (" + width + ", " + length + ", "+ height + ") is "  + surfaceArea);
+		
+		
+		
 
 	}
 
