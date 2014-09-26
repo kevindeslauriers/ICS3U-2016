2e54ecd644dbc025d94a2feee5db3e64d8461fa2
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-09-26 08:12:15 -0400

    Day One - Initial Commit

---
+++ Unit Two/src/com/bayviewglen/dayone/ExampleOne.java
@@ -0,0 +1,18 @@
+/* Math Class 
+ *  Math.PI
+ *  Math.pow(base, exponent)	-> Can take double as both the base and the exponent and returns a double as the answer
+ *  Math.sqrt(number)			-> Can take a double and returns the square root
+ *  Math.abs(number)			-> Returns the absolute value of the number -> there is an integer and double version 
+ *  							-> Math.abs(-2) returns 2
+ *  							-> Math.abs(-2.0) returns 2.0
+ */
+package com.bayviewglen.dayone;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Unit Two/src/com/bayviewglen/dayone/ExampleThree.java
@@ -0,0 +1,10 @@
+package com.bayviewglen.dayone;
+
+public class ExampleThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
