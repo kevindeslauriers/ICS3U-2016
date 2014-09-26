ab9f51dc3126c49b89512dc54d969c00c0ae4773
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-09-26 09:20:17 -0400

    Updated Math class lesson

--- Unit Two/src/com/bayviewglen/dayone/ExampleOne.java
+++ Unit Two/src/com/bayviewglen/dayone/ExampleOne.java
@@ -11,8 +11,38 @@ package com.bayviewglen.dayone;
 public class ExampleOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x = 2;
+		int y = -2;
+		double z = -2.0;
+	
+		
+		
+		System.out.println(x);
+		System.out.println(Math.sqrt(x));
+		
+		System.out.println(y);
+		System.out.println(Math.abs(y));
+		
+		System.out.println(z);
+		System.out.println(Math.abs(z));
+		
+		System.out.println(Math.sqrt(z));
+		System.out.println(Math.sqrt(y));
+		// System.out.println(Math.sqrt("Shon"));	// There is not a sqrt method/function that accepts Strings as arguments/parameters
+		
+		System.out.println(Math.sqrt(Math.abs(z)));
+		
+		System.out.println(Math.pow(z,2));
+		System.out.println(Math.pow(z,-2));
+		System.out.println(Math.pow(z, 1.2));	// because 1.2 is a radical
+		System.out.println(Math.pow(Math.abs(z), 1.2));	// because 1.2 is a radical
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.ceil(Math.PI));
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.round(Math.PI));
+		
+		
 	}
 
 }
