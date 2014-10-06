40e295daaf931d45b714772ccafe88e8a24012c4
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-10-06 10:36:51 -0400

    Updated example

--- Unit One/src/com/bayviewglen/dayeight/ExampleOne.java
+++ Unit One/src/com/bayviewglen/dayeight/ExampleOne.java
@@ -3,10 +3,11 @@ package com.bayviewglen.dayeight;
 public class ExampleOne {
 
 	public static void main(String[] args) {
-		byte x = 0b01111111;
-		byte y = 0b00000111;
+		byte x = 0b01001101;
+		byte y = 0b01111000;
 		
 		System.out.println((byte)(x+y));		// -122 = - (128 - 6) since 6 = 0000110 represents 6 and you get 10000110
+		System.out.println((x+y));		// -122 = - (128 - 6) since 6 = 0000110 represents 6 and you get 10000110
 		
 
 	}
