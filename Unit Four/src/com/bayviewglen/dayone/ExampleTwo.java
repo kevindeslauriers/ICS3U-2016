82b39a9408857dd0dfcf28d0562e35c4d564a626
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-11-12 08:16:52 -0500

    Initial Commit of Unit Four - Loops and Repetition

---
+++ Unit Four/src/com/bayviewglen/dayone/ExampleTwo.java
@@ -0,0 +1,16 @@
+/*
+ * Example: For Loop without an if statement.
+ * 
+ * Write a program that will sum all of numbers between two numbers that the user enters.
+ * 
+ */
+package com.bayviewglen.dayone;
+
+public class ExampleTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
