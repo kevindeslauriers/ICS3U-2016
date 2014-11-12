8d55c9e1106f2be48cd6096930318f3fa05ad9f3
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-11-12 09:48:58 -0500

    While and For loop examples

--- Unit Four/src/com/bayviewglen/dayone/ExampleOne.java
+++ Unit Four/src/com/bayviewglen/dayone/ExampleOne.java
@@ -6,11 +6,36 @@
  */
 package com.bayviewglen.dayone;
 
+import java.util.Scanner;
+
 public class ExampleOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Prompt the usrer for two numbers
+		Scanner keyboard = new Scanner(System.in);
 
+		System.out.print("Hey user give me two numbers, please: ");
+		int x = keyboard.nextInt();
+		int y = keyboard.nextInt();
+		
+		int low, high;
+		
+		if (x>y){
+			low = y;
+			high = x;
+		}else{
+			low = x;
+			high = y;
+		}
+		
+		int sum = 0;
+		while(low<=high){
+			sum+=low;	// sum = sum + low;
+			low++;		// low = low + 1;
+		}
+		
+		System.out.println(sum);
+		
 	}
 
 }
--- Unit Four/src/com/bayviewglen/dayone/ExampleTwo.java
+++ Unit Four/src/com/bayviewglen/dayone/ExampleTwo.java
@@ -6,10 +6,34 @@
  */
 package com.bayviewglen.dayone;
 
+import java.util.Scanner;
+
 public class ExampleTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Prompt the usrer for two numbers
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.print("Hey user give me two numbers, please: ");
+		int x = keyboard.nextInt();
+		int y = keyboard.nextInt();
+
+		int low, high;
+
+		if (x > y) {
+			low = y;
+			high = x;
+		} else {
+			low = x;
+			high = y;
+		}
+
+		int sum = 0;
+		for (int i = low; i <= high; i++) {
+			sum += i;
+		}
+
+		System.out.println(sum);
 
 	}
 
