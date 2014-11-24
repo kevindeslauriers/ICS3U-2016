50643215021dee99dd5258b7014f51f0579c8978
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-11-24 15:09:54 -0500

    Stub for BlackJack

---
+++ Unit Four/src/com/bayviewglen/dayfour/BlackJack.java
@@ -0,0 +1,32 @@
+package com.bayviewglen.dayfour;
+import java.util.Locale;
+
+
+public class BlackJack {
+
+	public static void main(String[] args) {
+		boolean done = false;
+		while (!done){
+			int wallet = 500;
+			String name = getUserName();
+			Locale l = getLocale();
+			int bet = getBet(wallet);
+		}
+	}
+
+	private static int getBet(int wallet) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static Locale getLocale() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static String getUserName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
