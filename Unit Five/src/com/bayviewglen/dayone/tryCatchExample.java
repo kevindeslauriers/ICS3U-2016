95dcd75a3a585a23c91fc724f4373e8ef3d2a2c0
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-11-28 09:10:45 -0500

    try catch example - will be helpful with betting and ensuring that you don't crash your program and that you can read input as a string and convert to an int.

---
+++ Unit Five/src/com/bayviewglen/dayone/tryCatchExample.java
@@ -0,0 +1,28 @@
+package com.bayviewglen.dayone;
+
+import java.util.Scanner;
+
+public class tryCatchExample {
+
+	public static void main(String[] args) {
+		
+		boolean validInput = false;
+		Scanner s = new Scanner (System.in);
+		
+		while(!validInput ){
+			System.out.print("Enter an integer: ");
+			String temp = s.nextLine();
+			int number = 0;
+			try{
+				number = Integer.parseInt(temp);
+				validInput = true;
+			}catch(NumberFormatException ex){
+				System.out.println(temp + " is not a number on earth in any language.");
+			}
+		}
+		
+		System.out.println("I am happy!!!!");
+
+	}
+
+}
