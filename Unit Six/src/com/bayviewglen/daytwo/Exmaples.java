bef4e037eb25f4939eadaa1204fcc7d9207c4459
kdeslauriers <kdeslauriers@bayviewglen.ca>
2015-01-09 08:36:18 -0500

    Added new remove method (with order) and display method.

--- Unit Six/src/com/bayviewglen/daytwo/Exmaples.java
+++ Unit Six/src/com/bayviewglen/daytwo/Exmaples.java
@@ -44,8 +44,29 @@ public class Exmaples {
 		size = removeNoOrder(numbers3, size, 0);
 		size = removeNoOrder(numbers3, size, 0);
 		
+		size = add(numbers3, size, 10);
+		size = add(numbers3, size, 20);
+		size = add(numbers3, size, 30);
+		size = add(numbers3, size, 40);
+		size = add(numbers3, size, 50);
+		size = add(numbers3, size, 60);
+		size = add(numbers3, size, 70);
+		size = add(numbers3, size, 80);
+		size = add(numbers3, size, 90);
+		
+		size = removeWithOrder(numbers3, size, 3);
+		
+		display(numbers3, size);
 	}
 	
+	private static int removeWithOrder(int[] arr, int size, int index) {
+		for (int i=index; i<size-1; ++i){
+			arr[i] = arr[i+1];
+		}
+		
+		return --size;
+	}
+
 	private static int removeNoOrder(int[] arr, int size, int index) {
 		if (index<0 || index> size-1)
 			return size;
@@ -72,10 +93,24 @@ public class Exmaples {
 	}
 	
 	public static void printEven(int[] arr){
+		System.out.println("---");
+		System.out.println("Print Even Starting");
 		for (int i = 0; i< arr.length; i++){
 			if (arr[i] % 2 == 0){
-				System.out.print(arr[i] + " ");
+				System.out.println(arr[i] + " ");
 			}
 		}
+		System.out.println("Print Even Ending");
+		System.out.println("---\n");
+	}
+	
+	public static void display(int[] arr, int size){
+		System.out.println("---");
+		System.out.println("Display Starting");
+		for (int i = 0; i<size; i++){
+				System.out.println(arr[i] + " ");
+		}
+		System.out.println("Display Ending");
+		System.out.println("---\n");
 	}
 }
