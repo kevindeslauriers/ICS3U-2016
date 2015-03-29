e00ec18fbce848f9542551e66dc9a25b9c55fcf1
kdeslauriers <kdeslauriers@bayviewglen.ca>
2015-03-29 15:06:32 -0400

    Updated solution so it is completed.  Now displays the coins that made the required value.
    ie 1 of 5    4 of 353    26 of 3553

--- ECOO Practise Questions/src/com/bayviewglen/regional2011/Problem3.java
+++ ECOO Practise Questions/src/com/bayviewglen/regional2011/Problem3.java
@@ -45,14 +45,19 @@ public class Problem3 {
 
 		while(input.hasNext()){	
 			
-			Integer finishAmount = Integer.valueOf(input.nextLine());
+			int finishAmount = Integer.valueOf(input.nextLine());
 			
 			//HashMap<Integer, Integer> solutions = new HashMap<Integer, Integer>();
 			int[] solutions = new int[finishAmount+1];
+			int[] solutions2 = new int[finishAmount+1];		// this is an array stating what coin got us to this solution.
+															// we can then just work backwords to get the coins of the previous 
+															// solutions.
+				
 			for (int i=3; i<=finishAmount; i++){		// let's use dynamic programming not memoization
-				if (validCoins.contains(i))
+				if (validCoins.contains(i)){
 					solutions[i] = 1;
-				else{	// check for the min by looking back after adding.
+					solutions2[i] = i;
+				}else{	// check for the min by looking back after adding.
 					int minValue = Integer.MAX_VALUE;	// everything is smaller than this
 					for(int j=0; j<coins.length; j++){
 						int value = coins[j];
@@ -60,6 +65,7 @@ public class Problem3 {
 							Integer temp = solutions[i - value];
 							if (temp != Integer.MAX_VALUE && temp < minValue){
 								minValue = temp + 1;
+								solutions2[i] = value;		// the coin that got us here!!!!
 							}
 						}
 					}
@@ -69,10 +75,40 @@ public class Problem3 {
 				}
 			}
 			
-			System.out.println(solutions[finishAmount] + " coins totalling " + finishAmount + ": ");
+			System.out.println(solutions[finishAmount] + " coins totalling " + finishAmount + ": " + displayCoins(finishAmount, solutions2));
 		}
 	}
 	
+	
+	private static String displayCoins(int finishAmount, int[] solutions2) {
+		int num3 = 0;
+		int num5 = 0;
+		int num53 = 0;
+		int num353 = 0;
+		int num3553 = 0;
+		
+		
+		int i = finishAmount;
+		while (i!=0){
+			if (solutions2[i] == 3) num3++;
+			if (solutions2[i] == 5) num5++;
+			if (solutions2[i] == 53) num53++;
+			if (solutions2[i] == 353) num353++;
+			if (solutions2[i] == 3553) num3553++;
+			i -= solutions2[i];
+		}
+		
+		String result = ""; 
+		
+		if (num3553 != 0) result += (num3553 + " of 3553  ");
+		if (num353 != 0) result += (num353 + " of 353  ");
+		if (num53 != 0) result += (num53 + " of 53  ");
+		if (num5 != 0) result += (num5 + " of 5  ");
+		if (num3 != 0) result += (num3 + " of 3  ");
+		
+		return result;
+	}
+
 	public static HashMap<Integer, Integer> solutions;
 	public static HashSet<Integer> validCoins;
 	public static int[] coins = {3553, 353, 53, 5, 3};
