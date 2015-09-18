dcde514c4920519516a654b138f5004212bb1df4
Kevin DesLauriers <kevindeslauriers@users.noreply.github.com>
2015-09-18 13:21:11 -0400

    Fixed formula to add PI

--- Unit One/src/com/bayviewglen/daythree/Homework2.java
+++ Unit One/src/com/bayviewglen/daythree/Homework2.java
@@ -10,7 +10,7 @@ public class Homework2 {
 		
 		radius = 3;
 		
-		volume = 4.0 / 3.0 * radius * radius * radius;	// we need double division since 4/3 is 1 and not 1.333...
+		volume = 4.0 / 3.0 * Math.PI * radius * radius * radius;	// we need double division since 4/3 is 1 and not 1.333...
 		
 		System.out.println("The volume of the sphere with radius " + radius + " is " + volume + ".");
 	}
