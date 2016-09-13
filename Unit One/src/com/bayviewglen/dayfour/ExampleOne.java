733216ee689dc27318966dcabcee35577cc8e5db
Kevin DesLauriers <kevindeslauriers@users.noreply.github.com>
2016-09-13 13:27:07 -0400

    div and mod

--- Unit One/src/com/bayviewglen/dayfour/ExampleOne.java
+++ Unit One/src/com/bayviewglen/dayfour/ExampleOne.java
@@ -19,7 +19,17 @@ public class ExampleOne {
 		System.out.println(23%2);	// 1 - this is how we can find out if it is an even or an odd
 		System.out.println(458548%10);	// 8 mod it by 10 to get the last digit
 		
-
+		int num = 292837;
+		int digit1 = num / 100000;
+		int digit2 = num / 10000 % 10;
+		int digit3 = num / 1000 % 10;
+		int digit4 = num / 100 % 10;
+		int digit5 = num / 10 % 10;
+		int digit6 = num % 10;
+		
+		int sum = digit1 + digit2 + digit3 + digit4 + digit5 + digit6;
+		System.out.println(sum);
+		
 
 	}
 
